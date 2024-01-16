03a775309c0635b789d011c57372ba79292e3550
Hkave88wr <119839508+Hkave88wr@users.noreply.github.com>
2024-01-16 11:16:58 +0800

    Update KhorosAPIException.scala

--- sp-ext/src/main/scala/com/paypal/csdmp/sp/ext/exception/KhorosAPIException.scala
+++ sp-ext/src/main/scala/com/paypal/csdmp/sp/ext/exception/KhorosAPIException.scala
@@ -1,7 +1,3 @@
 package com.paypal.csdmp.sp.ext.exception
 
-/**
- * @Author: Matthew Wu
- * @Date: 2023/1/9 14:45
- */
 case class KhorosAPIException(private val message: String = "", private val cause: Throwable = None.orNull) extends Exception(message, cause)
